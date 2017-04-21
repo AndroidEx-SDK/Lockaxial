70bb96e832e6d9112933175e5b2d941657e0ce05
hockgod <liyp@androidex.cn>
2017-04-21 10:06:31 +0800

    first commit.

---
+++ android/app/src/main/java/com/androidex/lockxial/config/DeviceConfig.java
@@ -0,0 +1,14 @@
+package com.androidex.lockxial.config;
+
+/**
+ * Created by simon on 2016/7/23.
+ */
+public class DeviceConfig {
+    /********residential*****/
+    public static String SERVER_URL="";
+    public static String SMS_APPKEY="1b36f1f6f0a34";
+    public static String SMS_APPSECRET="90eb1ad156572fe82e33f853440cea34";
+
+    public static final String LIFT_WIFI_SSID="CB_CTRL";
+    public static final String LIFT_WIFI_PASSWORD="12345678";
+}
